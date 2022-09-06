2e08a77680fe042d6d9c27244be5123ffa8b310f
johansmartinez <johan.martinez.dev@gmail.com>
2022-09-05 20:05:53 -0500

    Agregando datos con EF

--- Program.cs
+++ Program.cs
@@ -22,4 +22,15 @@ app.MapGet("/tareas", async ([FromServices] TareasContext dbContext)=>
 {
     return Results.Ok(dbContext.Tareas.Include(p=>p.Categoria).Where(p=>p.PrioridadTarea==proyef.Models.Prioridad.Baja));
 });
+
+app.MapPost("/tareas", async ([FromServices] TareasContext dbContext, [FromBody] proyef.Models.Tarea tarea)=>
+{
+    tarea.TareaId=Guid.NewGuid();
+    tarea.FechaCreacion= DateTime.Now;
+    await dbContext.AddAsync(tarea);
+    //await dbContext.Tareas.AddAsync(tarea);
+    
+    await dbContext.SaveChangesAsync();
+    return Results.Ok();
+});
 app.Run();
