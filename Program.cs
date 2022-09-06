2c521596a9b2ba0a804241f5e973961a03106c8e
johansmartinez <johan.martinez.dev@gmail.com>
2022-09-05 20:16:14 -0500

    Eliminando con EF

--- Program.cs
+++ Program.cs
@@ -50,4 +50,17 @@ app.MapPut("/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromB
     return Results.NotFound();
 });
 
+app.MapDelete("/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id)=>
+{
+    var tareaActual= dbContext.Tareas.Find(id);
+
+    if (tareaActual!=null)
+    {
+        dbContext.Remove(tareaActual);
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+    return Results.NotFound();
+});
+
 app.Run();
